1f9e881fb55c401de9b3fbfc60b9ebb12609805d
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-08-16 16:46:22 +0700

    feat(recipes): add bookmark items total

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
 import { useState, useEffect } from "react";
 
+const BookmarkButton = () => {
+  const [itemsTotal, setItemsTotal] = useState(0);
+  const items = JSON.parse(localStorage.getItem('bookmarks')) || [];
+
+  useEffect(() => {
+    setItemsTotal(items.length);
+  }, [items]); 
+
+  return (
+    <span className="relative">
+      <input type="checkbox" id="bookmark-checkbox" className="hidden" />
+      <label htmlFor="bookmark-checkbox" className="cursor-pointer rounded-full px-3 py-2 bg-slate-50">
+        <i className="text-neutral-800 text-xl fa-regular fa-bookmark"/>
+      </label>
+      <p className="font-semibold text-neutral-800 bg-yellow-400 px-2 rounded-full absolute -top-3 -right-2">{itemsTotal}</p>
+    </span>
+  )
+}
+
 const Navbar = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
   const [visible, setVisible] = useState(true);
@@ -10,9 +29,9 @@ const Navbar = () => {
       const header = document.querySelector("nav.nav-sticky");
 
       if (header) {
-        header.classList.toggle("bg-[#FDA403]", window.scrollY > 0);
-        header.classList.toggle("border-b", window.scrollY > 0);
-        header.classList.toggle("border-[#FF7700]", window.scrollY > 0);
+        header.classList.toggle("bg-[#FDA403]", currentScrollPos > 0);
+        header.classList.toggle("border-b", currentScrollPos > 0);
+        header.classList.toggle("border-[#FF7700]", currentScrollPos > 0);
       }
 
       setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
@@ -22,17 +41,14 @@ const Navbar = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [prevScrollPos, visible]);
+  }, [prevScrollPos]); 
 
   return (
     <nav className={`nav-sticky duration-700 z-10 fixed top-0 left-1/2 transform -translate-x-1/2 flex w-full max-w-[1500px] items-center justify-between p-5 px-5 md:px-10 border-transparent ${
           visible ? "translate-y-0" : "-translate-y-full"
         }`}>
       <img src="../img/Logo CS w text.png" className="w-40" alt="" />
-      <input type="checkbox" id="bookmark-checkbox" className="hidden" />
-      <label htmlFor="bookmark-checkbox" className="cursor-pointer rounded-full px-3 py-2 bg-slate-50">
-        <i className="text-neutral-800 text-xl fa-regular fa-bookmark"/>
-      </label>
+      <BookmarkButton/>
     </nav>
   )
 }
